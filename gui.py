91c643c26a2f7243e70c4ab8c44ebee354f17d08
jhsl2000 <74828742+jhsl2000@users.noreply.github.com>
2020-11-21 18:44:22 +0000

    Login
    janela de login atualizada + logo

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
 from tkinter import *
 import sys
 
-def command1():
+def command1(event):
     if entry1.get() == 'admin' and entry2.get() == 'password' or entry1.get() == 'test' and entry2.get() == 'pass':
         root.deiconify()
         top.destroy()
+
 
 def command2():
     top.destroy()
@@ -12,12 +13,12 @@ def command2():
 
 
 root = Tk()
-top = TopLevel()
-
-top.geometry('300x260')
+top = Toplevel()
+top.geometry('1280x720')
 top.title('Login screen')
 top.configure(background='white')
-
+photo2 = PhotoImage(file='NETFLOX.png')
+photo = Label(top, image=photo2, height=300, width= 250)
 label1 = Label(top, text="Username: ")
 entry1 = Entry(top)
 
@@ -28,16 +29,21 @@ button = Button(top, text='Cancel', command= lambda:command2())
 
 entry2.bind('<Return>', command1)
 
-label = Label(top, text='Copyright Jose Lourenco, Rodrigo Mendes', font={'Arial', 9})
+label3 = Label(top, text='Copyright Jose Lourenco, Rodrigo Mendes', font={'Arial', 9})
 
+photo.pack()
 label1.pack()
-label2.pack()
 entry1.pack()
+label2.pack()
 entry2.pack()
 button.pack()
+label3.pack()   
 
+root.title('Main Screen')
+root.configure(background="white")
+root.geometry('1280x720')
 
-
+root.withdraw()
 root.mainloop()
 
 
