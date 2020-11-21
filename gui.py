2772683765a6be0bcbc98b0fb05a4fd9ffe0774c
jhsl2000 <74828742+jhsl2000@users.noreply.github.com>
2020-11-21 20:00:38 +0000

    UI
    Feito no ficheiro ui o inicio do programa de login/registo

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import sys
 
+
 def command1(event):
     if entry1.get() == 'admin' and entry2.get() == 'password' or entry1.get() == 'test' and entry2.get() == 'pass':
         root.deiconify()
@@ -12,22 +13,39 @@ def command2():
     root.destroy()
 
 
+def command3():
+    top.destroy() 
+    root.deiconify()
+
+
+
 root = Tk()
 top = Toplevel()
 top.geometry('1280x720')
 top.title('Login screen')
 top.configure(background='white')
+
+
 photo2 = PhotoImage(file='NETFLOX.png')
 photo = Label(top, image=photo2, height=300, width= 250)
+
+button2 = Button(top, text='Registar', command=lambda: command3())
+button3 = Button(top, text='Login', command=lambda: command3())
+button2.pack()
+button3.pack()
+
+root.title('Main Screen')
+root.configure(background="white")
+root.geometry('1280x720')
+
 label1 = Label(top, text="Username: ")
 entry1 = Entry(top)
-
 label2 = Label(top, text="Password: ")
 entry2 = Entry(top, show="*")
 
-button = Button(top, text='Cancel', command= lambda:command2())
+button = Button(top, text='Cancel', command=lambda: command2())
 
-entry2.bind('<Return>', command1)
+entry2.bind('<Return>', command1)      # Tecla enter com a combinacao certa avança para a proxima janela
 
 label3 = Label(top, text='Copyright Jose Lourenco, Rodrigo Mendes', font={'Arial', 9})
 
@@ -42,6 +60,9 @@ label3.pack()
 root.title('Main Screen')
 root.configure(background="white")
 root.geometry('1280x720')
+
+
+
 
 root.withdraw()
 root.mainloop()
--- ui.py
+++ ui.py
@@ -1,20 +1,76 @@
 from tkinter import *
 
-root = Tk()
-root.title("NETFLOX")
 
-e = Entry(root, width=35, borderwidth=5)
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
-e.insert(0, "Enter your name: ")
+def register_user():
+    username_info = username.get()
+    password_info = password.get()
 
 
-def myClick():
-    hello = "Hello " + e.get()
-    myLabel = Label(root, text=hello)
-    myLabel.pack()
+def register():
+    global screen
+    screen1 = Toplevel(screen)
+    screen1.title("Registar")
+    screen1.geometry("1280x720")
+    photo2 = PhotoImage(file='NETFLOX.png')
+    photo = Label(screen, image=photo2, height=300, width=250)
+    photo.pack()
+
+    global username
+    global password
+    global username_entry
+    global password_entry
+
+    username=StringVar()
+    password=StringVar()
+    Label(screen1, text="Insere os detalhes em baixo: ").pack()
+    Label(screen1, text="").pack()
+    Label(screen1, text="Username: ").pack()
+    username_entry = Entry(screen1, textvariable = username)
+    username_entry.pack()
+    Label(screen1, text="Password: ").pack()
+    password_entry = Entry(screen1, textvariable = password, show="*")
+    password_entry.pack()
+    Label(screen1, text="").pack()
+    Button(screen1, text="Registar", command = register_user).pack()
 
 
-myButton = Button(root, text="Enter your name", command=myClick)
+def login():
+    global screen2
+    screen2 = Toplevel(screen)
+    screen2.title("Login")
+    screen2.geometry("1280x720")
+    Label(screen2, text= "Insere os detalhes para Login").pack()
+    Label(screen2, text="").pack()
+
+    global username_verify
+    global password_verify
+
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+    Label(screen2, text = "Username * ").pack()
+    username_verify = Entry(screen2, textvariable = username_verify)
+
+    Label(screen2, text = "").pack()
+    Label(screen2, text= "Password * ").pack()
+
+    Label(screen2, text="").pack()
+    Button(screen2, "Login").pack()
 
 
-root.mainloop()
+def main_screen():
+    global screen
+    screen = Tk()
+    screen.geometry("1280x720")
+    screen.title("Netflox 1.0")
+    photo2 = PhotoImage(file='NETFLOX.png')
+    photo = Label(screen, image=photo2, height=300, width=250)
+    photo.pack()
+    Button(text="Login", command=login).pack()
+    Label(text="").pack()
+    Button(text="Register", command=register).pack()
+
+    screen.mainloop()
+
+
+main_screen()
