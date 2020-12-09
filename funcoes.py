b9036688e2de41927b5533a799f12c20866fe020
jhsl2000 <74828742+jhsl2000@users.noreply.github.com>
2020-12-09 16:06:56 +0000

    basededadosv4

--- funcoes.py
+++ funcoes.py
@@ -8,7 +8,7 @@ def insere_novo_user(email_info, password_info, nome_info):
                                   password="postgres",
                                   host="localhost",
                                   port="5432",
-                                  database="ProjetoBD2020v3")
+                                  database="ProjetoBD2020v4")
         cursor = connection.cursor()
         cursor.execute(" INSERT INTO utilizador (email, password, nome, saldo) VALUES ('" +email_info +"','" +password_info +"','" +nome_info +"', '20')")
         print("Registado com sucesso")
@@ -31,7 +31,7 @@ def confirma_novo_user(email_info, password_info):
                                       password="postgres",
                                       host="localhost",
                                       port="5432",
-                                      database="ProjetoBD2020v3")
+                                      database="ProjetoBD2020v4")
         cursor = connection.cursor()
 
         cursor.execute(" SELECT email, password FROM utilizador WHERE utilizador.email ='" +email_info +"' AND password ='" +password_info +"'")
@@ -57,7 +57,7 @@ def check_login(email_entry1, password_entry1):
                                       password="postgres",
                                       host="localhost",
                                       port="5432",
-                                      database="ProjetoBD2020v3")
+                                      database="ProjetoBD2020v4")
         cursor = connection.cursor()
 
         cursor.execute("SELECT email, password FROM utilizador WHERE utilizador.email ='" +email_entry1 +"' AND password ='" +password_entry1 +"'")
@@ -89,7 +89,7 @@ def add_saldo(saldo_email_info, saldo_quantia_info):
                                       password="postgres",
                                       host="localhost",
                                       port="5432",
-                                      database="ProjetoBD2020v3")
+                                      database="ProjetoBD2020v4")
         cursor = connection.cursor()
         cursor.execute("Select email from utilizador where utilizador.email= '"+saldo_email_info +"'")
 
@@ -125,7 +125,7 @@ def consulta_saldo(email1):
                                       password="postgres",
                                       host="localhost",
                                       port="5432",
-                                      database="ProjetoBD2020v3")
+                                      database="ProjetoBD2020v4")
         cursor = connection.cursor()
         cursor.execute("Select saldo  from utilizador where utilizador.email= '"+email1 +"'")
 
@@ -149,7 +149,7 @@ def envia_mensagem(destinatario_info, assunto_info, mensagem_info):
                                       password="postgres",
                                       host="localhost",
                                       port="5432",
-                                      database="ProjetoBD2020v3")
+                                      database="ProjetoBD2020v4")
         cursor = connection.cursor()
         cursor.execute("SELECT email FROM UTILIZADOR WHERE email ='"+destinatario_info+"'")
 
